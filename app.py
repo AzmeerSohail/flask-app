d840d752a98006e6dd679a767763bf1b6016eabc
AzmeerSohail <azmeersohail013@gmail.com>
2025-02-18 14:18:10 +0500

    Changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deploying Flask App at Vercel by Azmeer"
+    return "Deploying Flask App at Vercel by Azmeer Sohail"
 
 if __name__ == '__main__':
     app.run(debug=True)
